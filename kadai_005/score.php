35b6826138a1cceae2d160175793e03f4a38afa2
KAHO-09 <kahosaegusa@gmail.com>
2025-06-20 11:23:11 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        $score_1 = 80;
+        $score_2 = 60;
+        $score_3 = 55;
+        $score_4 = 40;
+        $score_5 = 100;
+        $score_6 = 25;
+        $score_7 = 80;
+        $score_8 = 95;
+        $score_9 = 30;
+        $score_10 = 60;
 
+        // 平均点
+        $score_all = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+        echo $score_all / 10;
+        ?>
+    </p>
+</body>
+</html>
